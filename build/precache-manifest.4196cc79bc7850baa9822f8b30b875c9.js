822dbb16aac0285b6a77e05486982bec24b4b568
Jonathan Fox <FoxJonathanP@gmail.com>
2019-10-07 12:49:16 -0700

    fixing gh-pages

---
+++ build/precache-manifest.4196cc79bc7850baa9822f8b30b875c9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc2e9691ecdd8be7b090388e639381b",
+    "url": "/jfox-website-v2/index.html"
+  },
+  {
+    "revision": "f3b761e8ee00c3a87995",
+    "url": "/jfox-website-v2/static/css/main.39303e71.chunk.css"
+  },
+  {
+    "revision": "eb2af55d576ac3cb0c3e",
+    "url": "/jfox-website-v2/static/js/2.43c309c4.chunk.js"
+  },
+  {
+    "revision": "f3b761e8ee00c3a87995",
+    "url": "/jfox-website-v2/static/js/main.ca6ccf29.chunk.js"
+  },
+  {
+    "revision": "55b0f5e6314db1bc7cf3",
+    "url": "/jfox-website-v2/static/js/runtime~main.84fb128a.js"
+  },
+  {
+    "revision": "88578ca8f66555378867539bed83a725",
+    "url": "/jfox-website-v2/static/media/hs-lookup.88578ca8.png"
+  },
+  {
+    "revision": "62759eec2e0089d31953d4b51fe04217",
+    "url": "/jfox-website-v2/static/media/jet-spaceman.62759eec.png"
+  },
+  {
+    "revision": "e1d0a7baf52eb6e6a1bb6306748a6eaa",
+    "url": "/jfox-website-v2/static/media/outdoor-bg.e1d0a7ba.jpg"
+  },
+  {
+    "revision": "f1d8391d684767e82b74f0e8fb64955c",
+    "url": "/jfox-website-v2/static/media/pointer-mace.f1d8391d.png"
+  },
+  {
+    "revision": "c461abd027caba4d35fc9177019c3afb",
+    "url": "/jfox-website-v2/static/media/tft-tool.c461abd0.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FishWash/jfox-website-v2.git/precache-manifest.259ed01adecfe8d389951e8c3b42ab5d.js"
+  "/jfox-website-v2/precache-manifest.4196cc79bc7850baa9822f8b30b875c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FishWash/jfox-website-v2.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jfox-website-v2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
